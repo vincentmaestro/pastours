77ae330727d4025c985600abf08d7c61af7d37bd
vincentmaestro <maestroobika@gmail.com>
2024-01-16 23:49:42 +0100

    new

--- src/LoginAndSignup.jsx
+++ src/LoginAndSignup.jsx
@@ -18,14 +18,13 @@ export function VerifyEmail({auth, showPrompt}) {
 
     return (
         <div className="verify-email fixed top-0 w-full h-full z-[1] bg-[#001122e7]">
-            <form className="relative top-[30%] text-white" onSubmit={verifyEmail}>
-                <span className="material-symbols-outlined cursor-pointer relative left-[65%]" onClick={() => showPrompt(false)}>close</span>
+            <form className="relative top-[20%] text-white tablet:top-[10%]" onSubmit={verifyEmail}>
+                <span className="material-symbols-outlined cursor-pointer relative left-[85%]" onClick={() => showPrompt(false)}>close</span>
                 <h1 className="text-center font-bold text-3xl mt-[3%] mb-5">Verify Email</h1>
-                <div className="email flex flex-col gap-y-1 mb-4 w-[40%] mx-auto">
-                    {auth.currentUser && <p className="text-center pb-2 text-lg font-semibold">Dear {auth.currentUser.displayName}, we need you to verify your email to ease account operations</p>}
-                    {!verificationEmailSent ? <button type="submit" className="py-[4px] my-[4%] font-semibold text-lg bg-[#fd7e14] w-[45%] mx-auto rounded-[20px]">Send verification mail</button> 
-                    : <p className="text-xl font-semibold w-[55%] mx-auto">Verification email sent</p>
-                    }
+                <div className="email flex flex-col gap-y-1 mb-4 w-[45%] mx-auto laptop:w-[55%] tablet:w-[70%] mobile:w-[92%]">
+                    {auth.currentUser && <p className="text-center pb-2 text-lg font-semibold mobile:text-base">Dear {auth.currentUser.displayName}, we need you to verify your email to ease account operations.</p>}
+                    {!verificationEmailSent ? <button type="submit" className="py-[4px] my-[4%] font-semibold text-lg bg-[#fd7e14] w-[40%] mx-auto rounded-[20px] mobile:text-base mobile:py-[2px] mobile:w-[45%] mobile_m:w-[50%] mobile_s:w-[60%]">Send verification mail</button> 
+                    : <p className="text-xl font-semibold text-center">Check your mailbox!</p>}
                     {error && <p className="text-xl my-[2%] font-semibold mx-auto">An error occured</p>}
                 </div>
             </form>
@@ -248,51 +247,51 @@ function LoginandSignup({auth}) {
     return (
         <div className="loginandsignup fixed top-0 left-0 w-full h-full z-[1] bg-[#001122e7]">
             {authStage.login &&
-                <form className="login bg-orange-200 relative top-[7%] w-[65%] pt-[1%] pb-[5%] mx-auto laptop_s:top-[2%] laptop_l:w-[75%] laptop_l:top-[14%] tablet:top-[8%] tablet_s:w-[90%]" onSubmit={login}>
+                <form className="login bg-orange-200 relative top-[7%] w-[65%] pt-[1%] pb-[5%] mx-auto laptop_s:top-[4%] laptop_l:w-[75%] laptop_l:top-[14%] tablet:top-[8%] tablet_s:w-[90%] mobile:top-[10%]" onSubmit={login}>
                     <span className="material-symbols-outlined cursor-pointer relative left-[95%] tablet:left-[92%]" onClick={() => dispatch({case: 'start', state: false})}>close</span>
                     <h1 className="text-center font-bold text-3xl mt-[2%] mb-[4%] tablet:mt-0">Login</h1>
-                    <div className="email border flex flex-col gap-y-1 py-2 mb-6 w-[40%] mx-auto laptop_l:w-[45%] tablet:w-[60%] tablet:mb-[4%] tablet_s:w-[68%]">
+                    <div className="email border flex flex-col gap-y-1 py-2 mb-6 w-[40%] mx-auto laptop_l:w-[45%] tablet:w-[60%] tablet:mb-[4%] tablet_s:w-[68%] mobile_m:w-[78%] mobile_s:w-[85%]">
                         <span className="email-error block text-center text-sm text-red-500 font-semibold">{errors.email}</span>
                         <div className="flex justify-center gap-x-3">
                             <input type="email" name="email" placeholder="Email" id="savedEmail" className="w-[80%]" value={localStorage.getItem('rememberMe') ? localStorage.getItem('savedEmail') : loginData.email} onChange={handleLoginInput} />
                             <span className="material-symbols-outlined">mail</span>
                         </div>
                     </div>
-                    <div className="password border flex flex-col gap-y-1 py-2 mb-6 w-[40%] mx-auto laptop_l:w-[45%] tablet:w-[60%] tablet:mb-[3%] tablet_s:w-[68%]">
+                    <div className="password border flex flex-col gap-y-1 py-2 mb-6 w-[40%] mx-auto laptop_l:w-[45%] tablet:w-[60%] tablet:mb-[3%] tablet_s:w-[68%] mobile_m:w-[78%] mobile_s:w-[85%]">
                         <span className="password-error block text-center text-sm text-red-500 font-semibold">{errors.password}</span>
                         <div className="flex justify-center gap-x-3">
                             <input type={passwordVisible ? 'text' : 'password'} name="password" placeholder="Password" className="w-[80%]" value={loginData.password} onChange={handleLoginInput} />
                             <span className="material-symbols-outlined cursor-pointer" onClick={() => {setpasswordVisible(!passwordVisible)}}>{passwordVisible ? 'visibility' : 'visibility_off'}</span>
                         </div>
                     </div>
-                    <div className="remember-me flex gap-x-2 w-[40%] mx-auto mb-5 laptop_l:w-[45%] tablet:w-[60%] tablet:mb-[3%] tablet_s:gap-x-3 tablet_s:mb-[5%] tablet_s:w-[68%]">
+                    <div className="remember-me flex gap-x-2 w-[40%] mx-auto mb-5 laptop_l:w-[45%] tablet:w-[60%] tablet:mb-[3%] tablet_s:gap-x-3 tablet_s:mb-[5%] tablet_s:w-[68%] mobile_m:w-[78%]">
                         <input type="checkbox" defaultChecked = {localStorage.getItem('rememberMe') ? true : false} onChange={rememberMe} />
                         <p className="text-sm font-semibold">Remember me</p>
                     </div>
-                    <div className="flex justify-end gap-8 w-[40%] mx-auto mb-6 laptop_l:w-[45%] tablet:w-[60%] tablet:mb-[3%] tablet:gap-[10%] tablet_s:gap-[6%] tablet_s:w-[68%] tablet_s:mb-[4%]">
+                    <div className="flex justify-end gap-8 w-[40%] mx-auto mb-6 laptop_l:w-[45%] tablet:w-[60%] tablet:mb-[3%] tablet:gap-[10%] tablet_s:gap-[6%] tablet_s:w-[68%] tablet_s:mb-[4%] mobile_m:w-[78%] mobile_s:w-[85%]">
                         <button type="submit" className="py-[3px] px-[10px] rounded-[20px] bg-[#fd7e14] font-bold tablet:px-[14px] tablet:py-[1px]">Login</button>
                         <button className="text-sm font-semibold" onClick={() => toggleAuthStage('resetPassword')}>Forgot password?</button>
                     </div>
-                    <div className="flex items-center justify-between w-[40%] mx-auto mb-3 laptop_l:w-[45%] tablet:w-[60%] tablet:mb-[2%] tablet_s:w-[70%] tablet_s:mb-[1%]">
+                    <div className="flex items-center gap-x-[20%] w-[40%] mx-auto mb-3 laptop_l:w-[45%] tablet:w-[60%] tablet:gap-x-[18%] tablet:mb-[2%] tablet_s:mb-[1%] mobile:w-[75%] mobile:gap-x-[8%]">
                         <p className="text-sm font-medium">Don't have an account?</p>
                         <button className="font-semibold" onClick={() => toggleAuthStage('signup')}>Create Account</button>
                     </div>
                     <p className="text-center mb-3 font-semibold text-xl">OR</p>
-                    <button className="mx-auto block cursor-pointer"><img src="../src/assets/continuewithGoogle.svg" alt="continueWithGoogle" /></button>
+                    <button className="mx-auto block cursor-pointer"><img src="/src/assets/continuewithGoogle.svg" alt="continueWithGoogle" /></button>
                 </form>
             }
             {authStage.signup && 
-                <form className="signup bg-orange-200 relative top-[6%] w-[65%] pt-[1%] pb-[2%] mx-auto laptop_s:top-[1%] laptop_l:w-[75%] laptop_l:top-[12%] tablet:top-[4%] tablet_s:w-[90%]" onSubmit={handleSignup}>
+                <form className="signup bg-orange-200 relative top-[6%] w-[65%] pt-[1%] pb-[2%] mx-auto laptop_s:top-[4%] laptop_l:w-[75%] laptop_l:top-[12%] tablet:top-[4%] tablet_s:w-[90%]" onSubmit={handleSignup}>
                     <span className="material-symbols-outlined cursor-pointer relative left-[95%] tablet:left-[92%]" onClick={() => dispatch({case: 'start', state: false})}>close</span>
                     <h1 className="text-center font-bold text-3xl mb-[3%] laptop_s:mb-[2%] laptop_s:mt-[-2%]">Sign up</h1>
-                    <div className="email border flex flex-col gap-y-1 py-2 mb-4 w-[40%] mx-auto laptop_l:w-[45%] tablet:w-[60%] tablet:mb-[3%] tablet_s:w-[68%]">
+                    <div className="email border flex flex-col gap-y-1 py-2 mb-4 w-[40%] mx-auto laptop_l:w-[45%] tablet:w-[60%] tablet:mb-[3%] tablet_s:w-[68%] mobile_m:w-[78%] mobile_s:w-[85%]">
                         <span className="email-error block text-center text-sm text-red-500 font-semibold">{errors.email}</span>
                         <div className="flex justify-center gap-x-3">
                             <input type="email" name="email" placeholder="Email" className="w-[80%] py-[1px]" value={signupData.email} onChange={handleSignupInput} />
                             <span className="material-symbols-outlined">mail</span>
                         </div>
                     </div>
-                    <div className="username border flex flex-col gap-y-1 py-2 mb-4 w-[40%] mx-auto laptop_l:w-[45%] tablet:w-[60%] tablet:mb-[3%] tablet_s:w-[68%]">
+                    <div className="username border flex flex-col gap-y-1 py-2 mb-4 w-[40%] mx-auto laptop_l:w-[45%] tablet:w-[60%] tablet:mb-[3%] tablet_s:w-[68%] mobile_m:w-[78%] mobile_s:w-[85%]">
                         <span className="username-error block text-center text-sm text-red-500 font-semibold">{errors.username}</span>
                         <div className="flex justify-center gap-x-3">
                             <input type="text" name="username" placeholder="Username" className="w-[80%] py-[1px]" value={signupData.username} onChange={handleSignupInput} />
@@ -300,7 +299,7 @@ function LoginandSignup({auth}) {
                         </div>
                         <span className="text-xs font-semibold px-4">*minimum of 3 characters, maximum of 10. Can include letters, numbers, "-" and "."</span>
                     </div>
-                    <div className="password border flex flex-col gap-y-1 py-2 mb-4 w-[40%] mx-auto laptop_l:w-[45%] tablet:w-[60%] tablet:mb-[3%] tablet_s:w-[68%]">
+                    <div className="password border flex flex-col gap-y-1 py-2 mb-4 w-[40%] mx-auto laptop_l:w-[45%] tablet:w-[60%] tablet:mb-[3%] tablet_s:w-[68%] mobile_m:w-[78%] mobile_s:w-[85%]">
                         <span className="password-error block text-center text-sm text-red-500 font-semibold">{errors.password}</span>
                         <div className="flex justify-center gap-x-3">
                             <input type={passwordVisible ? 'text' : 'password'} name="password" placeholder="Password" className="w-[80%] py-[1px]" value={signupData.password} onChange={handleSignupInput} />
@@ -308,15 +307,13 @@ function LoginandSignup({auth}) {
                         </div>
                         <span className="text-xs font-semibold px-4">*minimum of 7 characters. Can include letters, numbers, "-" and "."</span>
                     </div>
-                    <div className="flex justify-center w-[35%] mx-auto mb-6 laptop_s:mb-[2%]">
-                        <button type="submit" className="py-[3px] px-[10px] rounded-[20px] bg-[#fd7e14] font-bold tablet:px-[12px] tablet:py-[1px]">Create Account</button>
-                    </div>
-                    <div className="flex items-center gap-x-[12%] w-[35%] mx-auto mb-2 laptop_l:w-[40%] tablet:w-[60%] laptop_s:mb-[1%] tablet:gap-[18%]">
+                    <button type="submit" className="w-[18%] ml-[40%] mb-2 py-[3px] rounded-[20px] bg-[#fd7e14] font-bold laptop_s:w-[26%] laptop_s:ml-[37%] tablet:ml-[35%] tablet:py-[1px] mobile:w-[36%] mobile:ml-[32%] mobile_m:w-[45%] mobile_m:ml-[28%]">Create Account</button>
+                    <div className="flex items-center gap-x-[12%] w-[35%] mx-auto mb-2 laptop_l:w-[40%] laptop_s:mb-[1%] tablet:w-[60%] tablet:gap-[24%] mobile_m:w-[72%] mobile_m:gap-[30%]">
                         <p className="text-sm font-medium">Already have an account?</p>
                         <button className="font-semibold" onClick={() => toggleAuthStage('login')}>Login</button>
                     </div>
                     <p className="text-center mb-3 font-semibold text-xl">OR</p>
-                    <button className="mx-auto block cursor-pointer"><img src="../src/assets/continuewithGoogle.svg" alt="continueWithGoogle" /></button>
+                    <button className="mx-auto block cursor-pointer"><img src="/src/assets/continuewithGoogle.svg" alt="continueWithGoogle" /></button>
                 </form>
             }
             {authStage.resetpassword && 
